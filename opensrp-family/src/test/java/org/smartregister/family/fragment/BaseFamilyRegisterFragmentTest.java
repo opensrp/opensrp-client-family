f5af130924997ac939c312f07a1df50417a1509f
Samuel Githengi <samuel.githengi@gmail.com>
2020-03-24 11:49:22 +0300

    Unit test setup views

--- opensrp-family/src/test/java/org/smartregister/family/fragment/BaseFamilyRegisterFragmentTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/fragment/BaseFamilyRegisterFragmentTest.java
@@ -1,9 +1,10 @@
 package org.smartregister.family.fragment;
 
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,12 +17,17 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.powermock.reflect.Whitebox;
 import org.robolectric.Robolectric;
+import org.robolectric.Shadows;
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
 import org.smartregister.commonregistry.CommonRepository;
 import org.smartregister.configurableviews.model.View;
 import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
 import org.smartregister.family.BaseUnitTest;
+import org.smartregister.family.R;
+import org.smartregister.view.contract.BaseRegisterFragmentContract;
+import org.smartregister.view.customcontrols.CustomFontTextView;
+import org.smartregister.view.customcontrols.FontVariant;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -49,6 +55,9 @@ public class BaseFamilyRegisterFragmentTest extends BaseUnitTest {
     @Mock
     public RecyclerView clientsView;
 
+    @Mock
+    private BaseRegisterFragmentContract.Presenter presenter;
+
     @Captor
     private ArgumentCaptor<RecyclerViewPaginatedAdapter> adapterArgumentCaptor;
 
@@ -57,22 +66,38 @@ public class BaseFamilyRegisterFragmentTest extends BaseUnitTest {
     private AppCompatActivity activity;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         registerFragment = Mockito.mock(BaseFamilyRegisterFragment.class, Mockito.CALLS_REAL_METHODS);
         CoreLibrary.init(context);
         when(context.commonrepository(anyString())).thenReturn(commonRepository);
-        activity= Robolectric.buildActivity(AppCompatActivity.class).create().get();
+        activity = Robolectric.buildActivity(AppCompatActivity.class).create().resume().get();
         when(registerFragment.getActivity()).thenReturn(activity);
-        Context.bindtypes= new ArrayList<>();
-        Whitebox.setInternalState(registerFragment,"clientsView",clientsView);
+        when(registerFragment.getContext()).thenReturn(activity);
+        Context.bindtypes = new ArrayList<>();
+        Whitebox.setInternalState(registerFragment, "clientsView", clientsView);
+        Whitebox.setInternalState(registerFragment, "presenter", presenter);
     }
 
     @Test
-    public void testInitializeAdapter(){
+    public void testInitializeAdapter() {
         registerFragment.initializeAdapter(new HashSet<View>());
         verify(clientsView).setAdapter(adapterArgumentCaptor.capture());
         assertNotNull(adapterArgumentCaptor.getValue());
-        assertEquals(20,adapterArgumentCaptor.getValue().currentlimit);
+        assertEquals(20, adapterArgumentCaptor.getValue().currentlimit);
 
+    }
+
+    @Test
+    public void testSetupViews() {
+        android.view.View view = LayoutInflater.from(activity).inflate(R.layout.fragment_base_register, null);
+        registerFragment.setupViews(view);
+        assertEquals(R.color.white, Shadows.shadowOf(registerFragment.getSearchView().getBackground()).getCreatedFromResId());
+        assertEquals(activity.getString(R.string.sort), ((TextView)view.findViewById(R.id.filter_text_view)).getText());
+        assertEquals(android.view.View .GONE, view.findViewById(R.id.opensrp_logo_image_view).getVisibility());
+
+
+        CustomFontTextView titleView = view.findViewById(R.id.txt_title_label);
+        assertEquals(android.view.View .VISIBLE, titleView.getVisibility());
+        assertEquals(activity.getString(R.string.all_families), titleView.getText());
     }
 }
