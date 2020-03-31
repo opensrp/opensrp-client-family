b2e00d66de2f62de3e2bb8a29b1ccab97b9cc3d9
Elly Kitoto <junkmailstoelly@gmail.com>
2020-03-31 12:02:56 +0300

    Start on adding test for NoMatchDialogFragment

---
+++ opensrp-family/src/test/java/org/smartregister/family/fragment/NoMatchDialogFragmentTest.java
@@ -0,0 +1,22 @@
+package org.smartregister.family.fragment;
+
+import android.app.FragmentManager;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.smartregister.family.BaseUnitTest;
+import org.smartregister.family.activity.BaseFamilyRegisterActivity;
+import org.smartregister.view.activity.BaseRegisterActivity;
+
+@Ignore("Not yet completed")
+public class NoMatchDialogFragmentTest extends BaseUnitTest {
+
+    @Test
+    public void launchDialog() {
+        BaseRegisterActivity familyRegisterActivity = Mockito.mock(BaseFamilyRegisterActivity.class, Mockito.CALLS_REAL_METHODS);
+        FragmentManager fragmentManager = Mockito.mock(FragmentManager.class);
+        Mockito.when(familyRegisterActivity.getFragmentManager()).thenReturn(fragmentManager);
+        NoMatchDialogFragment.launchDialog(familyRegisterActivity, NoMatchDialogFragment.class.getName(), "who-anc-id");
+    }
+}
