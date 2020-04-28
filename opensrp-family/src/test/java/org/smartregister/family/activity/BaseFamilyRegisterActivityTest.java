b479b00484886a6785157292743ab8c78cf070a2
Samuel Githengi <samuel.githengi@gmail.com>
2020-04-28 10:59:49 +0300

    test opening form displays toast on error

--- opensrp-family/src/test/java/org/smartregister/family/activity/BaseFamilyRegisterActivityTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/activity/BaseFamilyRegisterActivityTest.java
@@ -1,6 +1,7 @@
 package org.smartregister.family.activity;
 
 import android.content.Intent;
+import android.widget.Toast;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -10,15 +11,20 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.powermock.reflect.Whitebox;
 import org.robolectric.Robolectric;
+import org.robolectric.shadows.ShadowToast;
 import org.smartregister.Context;
 import org.smartregister.family.BaseUnitTest;
 import org.smartregister.family.FamilyLibrary;
+import org.smartregister.family.R;
 import org.smartregister.family.contract.FamilyRegisterContract;
 import org.smartregister.family.shadow.FamilyRegisterActivityShadow;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
 public class BaseFamilyRegisterActivityTest extends BaseUnitTest {
@@ -38,6 +44,7 @@ public class BaseFamilyRegisterActivityTest extends BaseUnitTest {
         Intent intent = new Intent();
         familyRegisterActivity = Robolectric.buildActivity(FamilyRegisterActivityShadow.class, intent).create().start().resume().get();
         Whitebox.setInternalState(familyRegisterActivity, "presenter", presenter);
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
     }
 
     @Test
@@ -48,9 +55,21 @@ public class BaseFamilyRegisterActivityTest extends BaseUnitTest {
 
     @Test
     public void testStartRegistration() throws Exception {
-        FamilyLibrary.getInstance().setMetadata(getMetadata());
         familyRegisterActivity.startRegistration();
-        verify(presenter).startForm("FAMILY_REGISTER",null,null,"");
+        verify(presenter).startForm("FAMILY_REGISTER", null, null, "");
+
+    }
+
+    @Test
+    public void testStartFormActivityDisplaysToastOnErrorOnError() throws Exception {
+        doThrow(new RuntimeException()).when(presenter).startForm(anyString(), anyString(), anyString(), anyString());
+        familyRegisterActivity.startFormActivity("FAMILY_REGISTER", "1234", "location3");
+        verify(presenter).startForm("FAMILY_REGISTER", "1234", "location3", "");
+        Toast toast = ShadowToast.getLatestToast();
+        assertNotNull(toast);
+        assertEquals(familyRegisterActivity.getString(R.string.error_unable_to_start_form), ShadowToast.getTextOfLatestToast());
+        assertEquals(Toast.LENGTH_LONG, toast.getDuration());
+
 
     }
 
