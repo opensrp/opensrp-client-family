b1bd53acb6848d518328a422158f3e7aa0ba5a4c
Samuel Githengi <samuel.githengi@gmail.com>
2020-05-12 11:08:05 +0300

    ViewPagerAdapter unit tests

---
+++ opensrp-family/src/test/java/org/smartregister/family/adapter/ViewPagerAdapterTest.java
@@ -0,0 +1,78 @@
+package org.smartregister.family.adapter;
+
+import android.database.DataSetObserver;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Pair;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
+import org.smartregister.family.BaseUnitTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by samuelgithengi on 5/12/20.
+ */
+public class ViewPagerAdapterTest extends BaseUnitTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private FragmentManager manager;
+
+    @Mock
+    private Fragment fragment;
+
+    @Mock
+    private DataSetObserver dataSetObserver;
+
+    private ViewPagerAdapter viewPagerAdapter;
+
+
+    @Before
+    public void setUp() {
+        viewPagerAdapter = new ViewPagerAdapter(manager);
+        viewPagerAdapter.addFragment(fragment, "fragment0");
+    }
+
+    @Test
+    public void testGetItem() {
+        assertEquals(fragment, viewPagerAdapter.getItem(0));
+    }
+
+
+    @Test
+    public void testGetCount() {
+        assertEquals(1, viewPagerAdapter.getCount());
+    }
+
+    @Test
+    public void testAddFragment() {
+        assertEquals(1, viewPagerAdapter.getCount());
+        viewPagerAdapter.addFragment(new Fragment(), "fragment1");
+        assertEquals(2, viewPagerAdapter.getCount());
+    }
+
+    @Test
+    public void testGetPageTitle() {
+        assertEquals("fragment0", viewPagerAdapter.getPageTitle(0));
+    }
+
+    @Test
+    public void testUpdateCount() {
+        Pair<Integer, Integer> pair = new Pair<>(10, 12);
+        viewPagerAdapter.registerDataSetObserver(dataSetObserver);
+        viewPagerAdapter.updateCount(pair);
+        assertEquals(pair, Whitebox.getInternalState(viewPagerAdapter, "mPositionCountPair"));
+        verify(dataSetObserver).onChanged();
+    }
+
+}
