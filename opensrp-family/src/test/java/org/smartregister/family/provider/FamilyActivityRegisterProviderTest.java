55d95e923d4a8b4e47c227e797effe29ebc82bc5
Samuel Githengi <samuel.githengi@gmail.com>
2020-05-12 12:03:24 +0300

    Unit test Populate PatientColumn With Dod

--- opensrp-family/src/test/java/org/smartregister/family/provider/FamilyActivityRegisterProviderTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/provider/FamilyActivityRegisterProviderTest.java
@@ -2,6 +2,7 @@ package org.smartregister.family.provider;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,11 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.RuntimeEnvironment;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.commonregistry.CommonRepository;
 import org.smartregister.family.BaseUnitTest;
 import org.smartregister.family.R;
 import org.smartregister.family.TestDataUtils;
-import org.smartregister.view.contract.SmartRegisterClient;
+import org.smartregister.family.util.DBConstants;
+import org.smartregister.family.util.Utils;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -50,18 +53,24 @@ public class FamilyActivityRegisterProviderTest extends BaseUnitTest {
 
     private FamilyActivityRegisterProvider.RegisterViewHolder viewHolder;
 
-    private SmartRegisterClient client = TestDataUtils.getCommonPersonObjectClient();
+    private CommonPersonObjectClient client = TestDataUtils.getCommonPersonObjectClient();
 
 
     private FamilyActivityRegisterProvider familyActivityRegisterProvider;
 
     private View rootView;
 
+    String ageString;
+
     @Before
     public void setUp() {
         familyActivityRegisterProvider = new FamilyActivityRegisterProvider(context, commonRepository, visibleColumns, onClickListener, paginationClickListener);
         rootView = familyActivityRegisterProvider.inflater.inflate(R.layout.family_activity_register_list_row, null);
         viewHolder = new FamilyActivityRegisterProvider.RegisterViewHolder(rootView);
+
+        String dob = Utils.getValue(client.getColumnmaps(), DBConstants.KEY.DOB, false);
+        String dobString = Utils.getDuration(dob);
+        ageString = dobString.contains("y") ? dobString.substring(0, dobString.indexOf("y")) : dobString;
     }
 
     @Test
@@ -72,8 +81,24 @@ public class FamilyActivityRegisterProviderTest extends BaseUnitTest {
         patientColumn.addView(textView);
         viewHolder = new FamilyActivityRegisterProvider.RegisterViewHolder(rootView);
         familyActivityRegisterProvider.getView(cursor, client, viewHolder);
-        assertEquals("Charity Otala, 38", viewHolder.patientNameAge.getText());
+        assertEquals("Charity Otala, " + ageString, viewHolder.patientNameAge.getText());
+        assertEquals(Color.BLACK, viewHolder.patientNameAge.getCurrentTextColor());
         assertEquals("Female", viewHolder.gender.getText());
+        assertEquals("Completed on 10 July 2018", viewHolder.lastVisit.getText());
+    }
+
+
+    @Test
+    public void testPopulatePatientColumnWithDod() {
+        String dod = "2019-03-05T00:00:00.000+03:00";
+        client.getColumnmaps().put(DBConstants.KEY.DOD, dod);
+
+        String dobString = Utils.getDuration(dod, client.getColumnmaps().get(DBConstants.KEY.DOB));
+        dobString = dobString.contains("y") ? dobString.substring(0, dobString.indexOf("y")) : dobString;
+
+        familyActivityRegisterProvider.getView(cursor, client, viewHolder);
+        assertEquals(String.format("Charity Otala, %s (deceased)", dobString), viewHolder.patientNameAge.getText());
+        assertEquals(Color.GRAY, viewHolder.patientNameAge.getCurrentTextColor());
         assertEquals("Completed on 10 July 2018", viewHolder.lastVisit.getText());
     }
 }
