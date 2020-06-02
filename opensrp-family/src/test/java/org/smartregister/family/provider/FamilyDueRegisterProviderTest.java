a47923fb650e61333fe8f23607418fadfb8c546a
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-02 12:20:59 +0300

    Add due provider unit tests

--- opensrp-family/src/test/java/org/smartregister/family/provider/FamilyDueRegisterProviderTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/provider/FamilyDueRegisterProviderTest.java
@@ -3,7 +3,9 @@ package org.smartregister.family.provider;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,10 +13,12 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
 import org.robolectric.RuntimeEnvironment;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.commonregistry.CommonRepository;
 import org.smartregister.family.BaseUnitTest;
+import org.smartregister.family.FamilyLibrary;
 import org.smartregister.family.R;
 import org.smartregister.family.TestDataUtils;
 import org.smartregister.family.util.DBConstants;
@@ -23,7 +27,14 @@ import org.smartregister.family.util.Utils;
 import java.util.HashSet;
 import java.util.Set;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 /**
  * Created by samuelgithengi on 6/2/20.
@@ -90,5 +101,124 @@ public class FamilyDueRegisterProviderTest extends BaseUnitTest {
         assertEquals(Color.GRAY, viewHolder.patientNameAge.getCurrentTextColor());
         assertEquals(View.GONE, viewHolder.nextArrow.getVisibility());
     }
+
+
+    @Test
+    public void testNextArrosOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.nextArrowColumn.performClick();
+        verify(onClickListener).onClick(viewHolder.nextArrow);
+    }
+
+
+    @Test
+    public void testStatusOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.statusColumn.performClick();
+        verify(onClickListener).onClick(viewHolder.patientColumn);
+    }
+
+    @Test
+    public void testPatientColumnOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.patientColumn.performClick();
+        verify(onClickListener).onClick(viewHolder.patientColumn);
+    }
+
+    @Test
+    public void testRegisterColumnsOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.registerColumns.performClick();
+        verify(onClickListener).onClick(viewHolder.patientColumn);
+    }
+
+
+    @Test
+    public void testUpdateClients() {
+        viewHolder = spy(viewHolder);
+        assertNull(provider.updateClients(null, null, null, null));
+        verifyZeroInteractions(viewHolder);
+    }
+
+    @Test
+    public void testOnServiceModeSelected() {
+        viewHolder = spy(viewHolder);
+        provider.onServiceModeSelected(null);
+        verifyZeroInteractions(viewHolder);
+    }
+
+    @Test
+    public void testNewFormLauncher() {
+        viewHolder = spy(viewHolder);
+        assertNull(provider.newFormLauncher(null, null, null));
+        verifyZeroInteractions(viewHolder);
+    }
+
+
+    @Test
+    public void testCreateViewHolder() {
+        viewHolder = provider.createViewHolder(null);
+        assertNotNull(viewHolder);
+        assertNotNull(viewHolder.patientNameAge);
+    }
+
+    @Test
+    public void testCreateFooterHolderIsHidden() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        Whitebox.setInternalState(Utils.metadata().familyDueRegister, "showPagination", false);
+        FamilyDueRegisterProvider.FooterViewHolder footer = (FamilyDueRegisterProvider.FooterViewHolder) provider.createFooterHolder(null);
+        assertNotNull(footer);
+        assertNotNull(footer.nextPageView);
+        assertNotNull(footer.previousPageView);
+        assertNotNull(footer.pageInfoView);
+        assertEquals(View.GONE, footer.itemView.getVisibility());
+    }
+
+
+    @Test
+    public void testCreateFooterHolderIsShown() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        RecyclerView.ViewHolder footer = provider.createFooterHolder(null);
+        assertNotNull(footer);
+        assertEquals(View.VISIBLE, footer.itemView.getVisibility());
+    }
+
+    @Test
+    public void testIsFooterViewHolder() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        assertTrue(provider.isFooterViewHolder(provider.createFooterHolder(null)));
+        assertFalse(provider.isFooterViewHolder(provider.createViewHolder(null)));
+    }
+
+    @Test
+    public void testGetFooterView(){
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        FamilyDueRegisterProvider.FooterViewHolder footer = (FamilyDueRegisterProvider.FooterViewHolder) provider.createFooterHolder(null);
+        provider.getFooterView(footer,2,12,true,true);
+        assertEquals("Page 2 of 12",footer.pageInfoView.getText());
+        assertEquals(View.VISIBLE,footer.nextPageView.getVisibility());
+        assertEquals(View.VISIBLE,footer.nextPageView.getVisibility());
+        footer.nextPageView.performClick();
+        verify(paginationClickListener).onClick(footer.nextPageView);
+
+        footer.previousPageView.performClick();
+        verify(paginationClickListener).onClick(footer.previousPageView);
+
+        //test if no previous and next page then controls are not rendered
+        provider.getFooterView(footer,2,12,false,false);
+        assertEquals(View.INVISIBLE,footer.nextPageView.getVisibility());
+        assertEquals(View.INVISIBLE,footer.nextPageView.getVisibility());
+
+
+    }
+
+
+    @Test
+    public void testFillValue(){
+        TextView textView= new TextView(context);
+        FamilyDueRegisterProvider.fillValue(textView,"Doe");
+        assertEquals("Doe",textView.getText());
+    }
+
 
 }
