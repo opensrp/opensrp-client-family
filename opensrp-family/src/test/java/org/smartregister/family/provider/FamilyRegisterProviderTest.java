5e50e87b40263b5fdc7a214fbc62177bc3349ed9
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-02 11:37:34 +0300

    Family register provider add pending unit tests

--- opensrp-family/src/test/java/org/smartregister/family/provider/FamilyRegisterProviderTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/provider/FamilyRegisterProviderTest.java
@@ -2,6 +2,7 @@ package org.smartregister.family.provider;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import org.junit.Before;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.powermock.reflect.Whitebox;
 import org.robolectric.RuntimeEnvironment;
 import org.smartregister.commonregistry.CommonPersonObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
@@ -19,14 +21,22 @@ import org.smartregister.family.FamilyLibrary;
 import org.smartregister.family.R;
 import org.smartregister.family.TestDataUtils;
 import org.smartregister.family.util.DBConstants;
+import org.smartregister.family.util.Utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 /**
@@ -93,6 +103,101 @@ public class FamilyRegisterProviderTest extends BaseUnitTest {
         assertEquals(View.VISIBLE, viewHolder.dueButton.getVisibility());
         assertEquals("Home Visit", viewHolder.dueButton.getText());
     }
+
+
+    @Test
+    public void testDueButtonOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.dueButton.performClick();
+        verify(onClickListener).onClick(viewHolder.dueButton);
+    }
+
+    @Test
+    public void testPatientColumnOnClick() {
+        provider.getView(cursor, client, viewHolder);
+        viewHolder.patientColumn.performClick();
+        verify(onClickListener).onClick(viewHolder.patientColumn);
+    }
+
+    @Test
+    public void testUpdateClients() {
+        viewHolder = spy(viewHolder);
+        assertNull(provider.updateClients(null, null, null, null));
+        verifyZeroInteractions(viewHolder);
+    }
+
+    @Test
+    public void testOnServiceModeSelected() {
+        viewHolder = spy(viewHolder);
+        provider.onServiceModeSelected(null);
+        verifyZeroInteractions(viewHolder);
+    }
+
+    @Test
+    public void testNewFormLauncher() {
+        viewHolder = spy(viewHolder);
+        assertNull(provider.newFormLauncher(null, null, null));
+        verifyZeroInteractions(viewHolder);
+    }
+
+
+    @Test
+    public void testCreateViewHolder() {
+        viewHolder = provider.createViewHolder(null);
+        assertNotNull(viewHolder);
+        assertNotNull(viewHolder.patientName);
+        assertNotNull(viewHolder.villageTown);
+    }
+
+    @Test
+    public void testCreateFooterHolderIsHidden() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        FamilyRegisterProvider.FooterViewHolder footer = (FamilyRegisterProvider.FooterViewHolder) provider.createFooterHolder(null);
+        assertNotNull(footer);
+        assertNotNull(footer.nextPageView);
+        assertNotNull(footer.previousPageView);
+        assertNotNull(footer.pageInfoView);
+    }
+
+
+    @Test
+    public void testCreateFooterHolderIsShown() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        Whitebox.setInternalState(Utils.metadata().familyActivityRegister, "showPagination", true);
+        RecyclerView.ViewHolder footer = provider.createFooterHolder(null);
+        assertNotNull(footer);
+        assertEquals(View.VISIBLE, footer.itemView.getVisibility());
+    }
+
+    @Test
+    public void testIsFooterViewHolder() {
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        assertTrue(provider.isFooterViewHolder(provider.createFooterHolder(null)));
+        assertFalse(provider.isFooterViewHolder(provider.createViewHolder(null)));
+    }
+
+    @Test
+    public void testGetFooterView(){
+        FamilyLibrary.getInstance().setMetadata(getMetadata());
+        FamilyRegisterProvider.FooterViewHolder footer = (FamilyRegisterProvider.FooterViewHolder) provider.createFooterHolder(null);
+        provider.getFooterView(footer,2,12,true,true);
+        assertEquals("Page 2 of 12",footer.pageInfoView.getText());
+        assertEquals(View.VISIBLE,footer.nextPageView.getVisibility());
+        assertEquals(View.VISIBLE,footer.nextPageView.getVisibility());
+        footer.nextPageView.performClick();
+        verify(paginationClickListener).onClick(footer.nextPageView);
+
+        footer.previousPageView.performClick();
+        verify(paginationClickListener).onClick(footer.previousPageView);
+
+        //test if no previous and next page then controls are not rendered
+        provider.getFooterView(footer,2,12,false,false);
+        assertEquals(View.INVISIBLE,footer.nextPageView.getVisibility());
+        assertEquals(View.INVISIBLE,footer.nextPageView.getVisibility());
+
+
+    }
+
 
 
 }
