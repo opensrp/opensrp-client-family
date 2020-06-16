8cffcb1a90bad01815df495b0d3efd8a7e640f5f
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-16 12:00:19 +0300

    Unit test saving an edited form

--- opensrp-family/src/test/java/org/smartregister/family/interactor/FamilyProfileInteractorTest.java
+++ opensrp-family/src/test/java/org/smartregister/family/interactor/FamilyProfileInteractorTest.java
@@ -1,18 +1,21 @@
 package org.smartregister.family.interactor;
 
 import org.apache.commons.lang3.tuple.Triple;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.powermock.reflect.Whitebox;
 import org.robolectric.util.ReflectionHelpers;
 import org.smartregister.Context;
+import org.smartregister.clientandeventmodel.Client;
+import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.commonregistry.CommonPersonObject;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.commonregistry.CommonRepository;
@@ -25,15 +28,16 @@ import org.smartregister.family.domain.FamilyEventClient;
 import org.smartregister.family.util.AppExecutors;
 import org.smartregister.family.util.Constants;
 import org.smartregister.family.util.DBConstants;
-import org.smartregister.family.util.Utils;
 import org.smartregister.repository.UniqueIdRepository;
+import org.smartregister.sync.helper.ECSyncHelper;
 
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -58,6 +62,15 @@ public class FamilyProfileInteractorTest extends BaseUnitTest {
 
     @Mock
     private Context context;
+
+    @Mock
+    private ECSyncHelper ecSyncHelper;
+
+    private String id = UUID.randomUUID().toString();
+
+    private Client client = new Client(id);
+
+    private Event event = new Event();
 
     @Captor
     private ArgumentCaptor<CommonPersonObjectClient> commonPersonObjectClientArgumentCaptor;
@@ -94,6 +107,7 @@ public class FamilyProfileInteractorTest extends BaseUnitTest {
         commonPersonObject = new CommonPersonObject("some-crazy-base-entity-id", "", null, "");
         commonPersonObject.setColumnmaps(TestDataUtils.getCommonPersonObjectClient().getColumnmaps());
         FamilyLibrary.getInstance().setMetadata(getMetadata());
+        event.setBaseEntityId(id);
     }
 
 
@@ -113,55 +127,22 @@ public class FamilyProfileInteractorTest extends BaseUnitTest {
     }
 
     @Test
-    public void testSaveRegistration() {
-        FamilyEventClient familyEventClient = Mockito.mock(FamilyEventClient.class);
-        String someJson = "{\n" +
-                "  \"count\": \"2\",\n" +
-                "  \"encounter_type\": \"Family Registration\",\n" +
-                "  \"entity_id\": \"\",\n" +
-                "  \"relational_id\": \"\",\n" +
-                "  \"step1\": {\n" +
-                "    \"title\": \"Family member registration\",\n" +
-                "    \"fields\": [\n" +
-                "{\n" +
-                "        \"key\": \"unique_id\",\n" +
-                "        \"openmrs_entity_parent\": \"\",\n" +
-                "        \"openmrs_entity\": \"person_identifier\",\n" +
-                "        \"openmrs_entity_id\": \"opensrp_id\",\n" +
-                "        \"type\": \"edit_text\",\n" +
-                "        \"hint\": \"ID\",\n" +
-                "        \"read_only\": \"True\",\n" +
-                "        \"v_required\": {\n" +
-                "          \"value\": \"true\",\n" +
-                "          \"err\": \"Please enter the ID\"\n" +
-                "        }\n" +
-                "      },\n" +
-                "      {\n" +
-                "        \"key\": \"first_name\",\n" +
-                "        \"openmrs_entity_parent\": \"\",\n" +
-                "        \"openmrs_entity\": \"\",\n" +
-                "        \"openmrs_entity_id\": \"\",\n" +
-                "        \"type\": \"edit_text\",\n" +
-                "        \"hint\": \"First name\",\n" +
-                "        \"edit_type\": \"name\",\n" +
-                "        \"v_required\": {\n" +
-                "          \"value\": \"true\",\n" +
-                "          \"err\": \"Please enter the first name\"\n" +
-                "        },\n" +
-                "        \"relevance\": {\n" +
-                "          \"rules-engine\": {\n" +
-                "            \"ex-rules\": {\n" +
-                "              \"rules-file\": \"family-member-relevance.yml\"\n" +
-                "            }\n" +
-                "          }\n" +
-                "        }\n" +
-                "      }" +
-                "    ]\n" +
-                "  }\n" +
-                "}";
+    public void testSaveRegistrationNonEditMode() {
+        FamilyEventClient familyEventClient = new FamilyEventClient(client, event);
+        familyProfileInteractor.saveRegistration(familyEventClient, TestDataUtils.FILLED_FAMILY_FORM, false, familyProfileCallback);
+        verify(familyProfileCallback, timeout(ASYNC_TIMEOUT)).onRegistrationSaved(false, true, familyEventClient);
+    }
 
-        familyProfileInteractor.saveRegistration(familyEventClient, someJson, false, familyProfileCallback);
-        verify(familyProfileCallback, timeout(ASYNC_TIMEOUT)).onRegistrationSaved(eq(false), eq(false), eq(familyEventClient));
+    @Test
+    public void testSaveRegistrationEditMode() throws JSONException {
+        Whitebox.setInternalState(FamilyLibrary.getInstance(),"syncHelper",ecSyncHelper);
+        when(ecSyncHelper.getClient(id)).thenReturn(new JSONObject("{\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"birthdate\":\"1997-11-21T07:00:00.000+07:00\",\"birthdateApprox\":false,\"deathdateApprox\":false,\"gender\":\"Female\",\"relationships\":{\"family\":[\"9d4c4722-eef4-4baa-94aa-1805b2a0a60b\"]},\"baseEntityId\":\"600d9823-78ae-48e2-8c4c-c9\",\"identifiers\":{\"opensrp_id\":\"11152030\"},\"addresses\":[],\"attributes\":{\"residence\":\"896d12ca-2ac8-4e7c-a725-cd42ea49ac06\"},\"dateCreated\":\"2019-11-21T17:13:33.197+07:00\",\"serverVersion\":1574331213134,\"clientApplicationVersion\":7,\"clientDatabaseVersion\":3,\"type\":\"Client\",\"id\":\"b43ea939-3e54-45b3-9197-cb5839c8518a\",\"revision\":\"v1\"}"));
+        FamilyEventClient familyEventClient = new FamilyEventClient(client, event);
+        //client.addIdentifier("UNIQUE_IDENTIFIER_KEY","123");
+        familyProfileInteractor.saveRegistration(familyEventClient, TestDataUtils.FILLED_FAMILY_FORM, true, familyProfileCallback);
+        verify(familyProfileCallback, timeout(ASYNC_TIMEOUT)).onRegistrationSaved(true, true, familyEventClient);
+        verify(ecSyncHelper).addClient(eq(id),any(JSONObject.class));
+        verify(ecSyncHelper).addEvent(eq(id),any(JSONObject.class));
     }
 
     @Test
